cacf9857591e77ac4823c055972047e167f37623
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-12-24 14:21:24 +0700

    update time

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function clock(){
-    var days = document.getElementById('days');
-    var hours = document.getElementById('hour');
-    var minutes = document.getElementById('minutes');
-    var seconds = document.getElementById('seconds');
+function clock() {
+  var days = document.getElementById("days");
+  var hours = document.getElementById("hour");
+  var minutes = document.getElementById("minutes");
+  var seconds = document.getElementById("seconds");
 
-    var publishDate = new Date("Sep 30, 2020 22:30:30").getTime();
-    var now = new Date().getTime();
+  var publishDate = new Date("Jan 1, 2021 00:00:00").getTime();
+  var now = new Date().getTime();
 
-    var remainingTime = publishDate - now;
+  var remainingTime = publishDate - now;
 
-    var d = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-    var h = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var m = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-    var s = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  var d = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  var h = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var m = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  var s = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-    days.innerHTML = d;
-    hours.innerHTML = h;
-    minutes.innerHTML = m;
-    seconds.innerHTML = s;
+  days.innerHTML = d;
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
 }
 
-var interval= setInterval(clock, 1000);
+var interval = setInterval(clock, 1000);
 
 interval;
